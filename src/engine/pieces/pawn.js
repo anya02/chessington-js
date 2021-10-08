fea54e1c4786b5101b6e6162e94a68ae0455ce41
Anya Murphy <anya.murphy@mac.com>
2021-10-08 13:41:42 +0100

    Pawns can move 2 spaces on first move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -6,15 +6,25 @@ export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
-
+    
     getAvailableMoves(board) {
         const currentSquare = board.findPiece(this);
 
         if (this.player === Player.WHITE) {
-            return [Square.at(currentSquare.row + 1, currentSquare.col)];
+
+            if (this.moveCounter < 1) {
+                return [Square.at(currentSquare.row + 1, currentSquare.col), Square.at(currentSquare.row + 2, currentSquare.col)];
+            }
+
+            return [Square.at(currentSquare.row + 1, currentSquare.col)]
         }
+
         if (this.player === Player.BLACK) {
-            return [Square.at(currentSquare.row - 1, currentSquare.col)];
+            if (this.moveCounter < 1) {
+                return [Square.at(currentSquare.row - 1, currentSquare.col), Square.at(currentSquare.row - 2, currentSquare.col)];
+            }
+
+            return [Square.at(currentSquare.row - 1, currentSquare.col)]
         }
     }
 }
