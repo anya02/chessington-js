0b7169ec5d071c7ef415020ed4f37b9c46aee7e4
Anya Murphy <anya.murphy@mac.com>
2021-10-08 10:57:18 +0100

    Pawns can move one space

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
+import Player from '../player';
 import Piece from './piece';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,13 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+
+        if (this.player === Player.WHITE) {
+            return [Square.at(currentSquare.row + 1, currentSquare.col)];
+        }
+        if (this.player === Player.BLACK) {
+            return [Square.at(currentSquare.row - 1, currentSquare.col)];
+        }
     }
 }
