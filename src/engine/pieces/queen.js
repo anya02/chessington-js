41422f60f12dc5fd59f495f06245b36538424d84
Anya Murphy <anya.murphy@mac.com>
2021-10-11 12:11:19 +0100

    Queen can move laterally and diagonally

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,59 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const possibleLocations = [];
+        const currentRow = board.board[currentSquare.row];
+        const upperBound = board.board.length;
+        const lowerBound = -1;
+
+        for (let x = 0; x < board.board.length; x++) {
+            const possibleSquare = Square.at(currentSquare.row, x);
+
+            if (!currentSquare.equals(possibleSquare)) {
+                possibleLocations.push(possibleSquare);
+            }
+        }
+
+        for (let x = 0; x < currentRow.length; x++) {
+            const possibleSquare = Square.at(x, currentSquare.col);
+
+            if (!currentSquare.equals(possibleSquare)) {
+                possibleLocations.push(possibleSquare);
+            }
+        }
+
+        for (let x = 0; x < board.board.length; x++) {
+            const newSquare = new Square.at(currentSquare.row + x + 1, currentSquare.col + x + 1);
+
+            if (newSquare.row < upperBound && newSquare.col < upperBound) {
+                possibleLocations.push(newSquare);
+            }
+        }
+
+        for (let x = 0; x < board.board.length; x++) {
+            const newSquare = new Square.at(currentSquare.row + x + 1, currentSquare.col - x - 1);
+
+            if (newSquare.row < upperBound && newSquare.col > lowerBound) {
+                possibleLocations.push(newSquare);
+            }
+        }
+
+        for (let x = 0; x < board.board.length; x++) {
+            const newSquare = new Square.at(currentSquare.row - x - 1, currentSquare.col + x + 1);
+
+            if (newSquare.row > lowerBound && newSquare.col < upperBound) {
+                possibleLocations.push(newSquare);
+            }
+        }
+
+        for (let x = 0; x < board.board.length; x++) {
+            const newSquare = new Square.at(currentSquare.row - x - 1, currentSquare.col - x - 1);
+
+            if (newSquare.row > lowerBound && newSquare.col > lowerBound) {
+                possibleLocations.push(newSquare);
+            }
+        }
+        return possibleLocations;
     }
 }
